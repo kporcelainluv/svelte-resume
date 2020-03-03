abcc79795c41230e452f4391745d4a333537876c
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-03 15:43:29 +0300

    update html

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
 <script>
+  import List from "./List.svelte";
+  import Projects from "./Projects.svelte";
+
   let avatar = "./static/avatar.jpg";
   let html1 = "./static/html1.pdf";
   let html2 = "./static/html2.pdf";
@@ -7,21 +10,90 @@
   let coursera2 = "./static/Coursera2.pdf";
   let stepic = "./static/stepik.pdf";
   let loftschool = "./static/loftschool.pdf";
-  let freecodecamp = "https://www.freecodecamp.org/certification/kporcelainluv/javascript-algorithms-and-data-structures"
+  let freecodecamp =
+    "https://www.freecodecamp.org/certification/kporcelainluv/javascript-algorithms-and-data-structures";
+
+  const skills = [
+    "Responsive HTML/CSS",
+    "SASS",
+    "Javascript",
+    "React/Redux",
+    "Firebase",
+    "Styled components"
+  ];
+
+  const social = [
+    { name: "Github", link: "https://github.com/kporcelainluv" },
+    { name: "Telegram", link: "https://t.me/ksushaz" },
+    { name: "zhukovaksusha@gmail.com", link: "mailto:zhukovaksusha@gmail.com" }
+  ];
+
+  const courses = [
+    { name: "HTML Academy, HTML/CSS 1", link: html1 },
+    { name: "HTML Academy, HTML/CSS 2", link: html2 },
+    { name: "HTML Academy, JavaScript 2", link: js2 },
+    {
+      name: "FreeCodeCamp, JS Algorithms and Data Structures",
+      link: freecodecamp
+    },
+    { name: "Frontend masters, Complete Intro to React", link: "" },
+    { name: "Udemy, Modern React with Redux", link: "" },
+    { name: "Coursera, Introduction to Python", link: coursera1 },
+    { name: "Coursera, Diving into Python", link: coursera2 },
+    { name: "Stepik.org, Programming on Python", link: stepic },
+    { name: "Loftschool, Web-development for beginners", link: loftschool }
+  ];
+
+  const projects = [
+    {
+      name: "HTML Academy, HTML/CSS 1",
+      link: "https://github.com/kporcelainluv/226838-device",
+      desc: "Introduction to HTML and CSS project. "
+    },
+    {
+      name: "HTML Academy, HTML/CSS 2",
+      link: "https://github.com/kporcelainluv/226838-sedona-17",
+      desc: "HTML/SASS project. Uses Gulp and BAM."
+    },
+    {
+      name: "HTML Academy, JS 2",
+      link: "https://github.com/kporcelainluv/226838-cinemaddict-10",
+      desc:
+        "Object-oriented programming project. Moowle website fetches favorite films from server and displays to user. Favorite films, history, statistics pages are available."
+    },
+    {
+      name: "Cinemaddict project in React.js",
+      link: "https://github.com/kporcelainluv/cinemaddict-react-app",
+      desc: "Project written with React state."
+    },
+    {
+      name: "Questionary project in React.js",
+      link: "https://github.com/kporcelainluv/questionaryProject",
+      desc:
+        "Questionary made for business to gather data. Has a list of users and user profile avaliable for whitelist users. Uses Firebase to save data. Connected to Telegram bot."
+    },
+    {
+      name: "React exercises",
+      link: "https://github.com/kporcelainluv/react-practice",
+      desc:
+        "Simple projects in React.js like To-do list, Counter, Fetch API, Current time"
+    }
+  ];
 </script>
 
 <style>
-  .container {
+  container {
     max-width: 900px;
     margin: 0 auto;
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
     padding: 32px;
     font-family: "Raleway", Roboto, sans-serif;
     font-size: 18px;
     line-height: 32px;
-    flex-direction: column;
+    color: #181919;
   }
   visually-hidden {
     position: absolute;
@@ -33,149 +105,77 @@
     clip: rect(0 0 0 0);
     overflow: hidden;
   }
-  img {
-    justify-content: center;
+  block {
+    align-items: center;
+    flex-direction: column;
   }
-  h1 {
-    margin-bottom: 8px;
+  .centered {
+    text-align: center;
+  }
+  .main {
+    width: 400px;
   }
   h2 {
     margin: 0 0 8px;
   }
   h3 {
     font-weight: bold;
+    margin-bottom: 0;
   }
-  a {
-    color: black;
+  ul {
+    margin-top: 8px;
   }
-  a:hover {
-    color: #333;
-  }
-  a:focus {
-    color: #333;
-  }
-  block {
-    align-items: center;
-    flex-direction: column;
-  }
-  @media (max-width: 800px) {
+  @media (max-width: 500px) {
     container {
-      font-size: 14px;
-      padding: 16px;
+      font-size: 16px;
     }
     .avatar {
       width: 100px;
       height: 100px;
     }
+    .main {
+      width: 300px;
+    }
   }
 </style>
 
-<div class="container">
-  <visually-hidden className="visually-hidden">
-    Xenia Zhukova's website
-  </visually-hidden>
+<container>
+  <div class="main">
+    <visually-hidden className="visually-hidden">
+      Xenia Zhukova's website
+    </visually-hidden>
+    <div class="centered">
+      <img
+        class="avatar"
+        src={avatar}
+        width="120"
+        height="120"
+        alt="Xenia Zhukova's photo" />
+    </div>
 
-  <img
-    class="avatar"
-    src={avatar}
-    width="120"
-    height="120"
-    alt="Xenia Zhukova's photo" />
-
-  <block>
-    <h1>Xenia Zhukova</h1>
-    <h2>Front-end developer</h2>
-  </block>
-  <block>
-    <h3>Skills</h3>
-    <ul>
-      <li>Responsive HTML/CSS</li>
-      <li>SASS</li>
-      <li>Javascript</li>
-      <li>React/Redux</li>
-      <li>Firebase</li>
-      <li>Styled components</li>
-    </ul>
-  </block>
-  <block>
-    <h3>Social</h3>
-    <ul>
-      <li>
-        <a href="https://github.com/kporcelainluv">Github</a>
-      </li>
-      <li>
-        <a href="https://t.me/ksushaz">Telegram</a>
-      </li>
-      <li>
-        <a href="mailto:zhukovaksusha@gmail.com">zhukovaksusha@gmail.com</a>
-      </li>
-    </ul>
-  </block>
-  <block>
-    <h3>Projects</h3>
-    <ul>
-      <li>
-        <a href="https://github.com/kporcelainluv/226838-device">
-          HTML Academy, HTML/CSS 1
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/kporcelainluv/226838-sedona-17">
-          HTML Academy, HTML/CSS 2
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/kporcelainluv/226838-cinemaddict-10">
-          HTML Academy, JS 2
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/kporcelainluv/cinemaddict-react-app">
-          Cinemaddict project in React.js
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/kporcelainluv/questionaryProject">
-          Questionary project in React.js
-        </a>
-      </li>
-    </ul>
-  </block>
-  <block>
-    <h3>Certificates</h3>
-    <ul>
-      <li>
-        <a {html1}>HTML Academy, HTML/CSS 1</a>
-      </li>
-      <li>
-        <a {html2}>HTML Academy, HTML/CSS 2</a>
-      </li>
-      <li>
-        <a {js2}>HTML Academy, Java Script 2</a>
-      </li>
-      <li>
-        <a {freecodecamp}>
-          FreeCodeCamp, JavaScript Algorithms and Data Structures
-        </a>
-      </li>
-      <li>
-        <a {`#`}>Frontend masters, Complete Intro to React</a>
-      </li>
-      <li>
-        <a {`#`}>Udemy, Modern React with Redux</a>
-      </li>
-      <li>
-        <a {coursera1}>Coursera, Introduction to Python</a>
-      </li>
-      <li>
-        <a {coursera2}>Coursera, Diving into Python</a>
-      </li>
-      <li>
-        <a {stepic}>Stepik.org, Programming on Python</a>
-      </li>
-      <li>
-        <a {loftschool}>Loftschool, Web-development for beginners</a>
-      </li>
-    </ul>
-  </block>
-</div>
+    <div class="centered">
+      <h1>Xenia Zhukova</h1>
+      <h2>Front-end developer</h2>
+    </div>
+    <block>
+      <h3>Social</h3>
+      <List elements={social} />
+    </block>
+    <block>
+      <h3>Skills</h3>
+      <ul>
+        {#each skills as skill}
+          <li>{skill}</li>
+        {/each}
+      </ul>
+    </block>
+    <block>
+      <h3>Projects</h3>
+      <Projects {projects} />
+    </block>
+    <block>
+      <h3>Certificates</h3>
+      <List elements={courses} />
+    </block>
+  </div>
+</container>
