f1e52f83735f4e1ed38378b7a659448023128080
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-03 15:42:51 +0300

    add additional components

---
+++ src/List.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let elements;
+</script>
+
+<style>
+  a {
+    color: black;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+  a:hover {
+    color: grey;
+  }
+  a:focus {
+    color: grey;
+  }
+  ul {
+    margin-top: 8px;
+  }
+</style>
+
+<ul>
+  {#each elements as element}
+    <li>
+      <a href={element.link}>{element.name}</a>
+    </li>
+  {/each}
+</ul>
---
+++ src/Projects.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let projects;
+</script>
+
+<style>
+  a {
+    color: black;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+  a:hover {
+    color: grey;
+  }
+  a:focus {
+    color: grey;
+  }
+  ul {
+    margin-top: 8px;
+  }
+  p {
+    padding-left: 16px;
+    font-size: 16px;
+    margin: 6px 0px 12px;
+    line-height: 22px;
+  }
+</style>
+
+<ul>
+  {#each projects as project}
+    <li>
+      <a href={project.link}>{project.name}</a>
+      <p>{project.desc}</p>
+    </li>
+  {/each}
+</ul>
