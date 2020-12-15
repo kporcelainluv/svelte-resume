a19b3edef216deb8ae9c2415e4d26c37d9cd0a3b
Zhukova Xenia <zhukovaksusha@gmail.com>
2020-12-16 00:41:27 +0300

    Refactor component

--- src/WorkExperience.svelte
+++ src/WorkExperience.svelte
@@ -3,44 +3,78 @@
 </script>
 
 <style>
-    .block-heading {
-        padding-left: 0;
-        font-weight: 500;
-        color: #242535;
-        text-decoration: none;
-        font-size: 18px;
-        margin-bottom: 16px;
-    }
-
-    .additional {
-        font-size: 12px;
-        font-style: italic;
-        color: rgba(0, 0, 0, 0.5);
-        line-height: 16px;
-    }
-
-    .description {
-        font-size: 16px;
-        line-height: 22px;
-        color: rgba(0, 0, 0, 0.7);
-        margin: 0 0 8px;
-    }
-
+    
     .jobs {
         margin: 0;
     }
+    .job {
+        margin-bottom: 16px;
+    }
+     .heading {
+        color: #242535;
+        font-weight: 500;
+        text-decoration: none;
+        font-size: 18px;
+        display: inline-block;
+        margin: 0 0 4px;
+    }
+    .dates {
+        display: flex;
+        margin-bottom: 8px;
+    }
+    
+    .date {
+        display: inline-flex;
+        align-items: center;
+        font-size: 14px;
+        line-height: 1.5;
+    }
+    .description-block {
+        list-style: none;
+        padding-left: 0;
+        margin-bottom: 8px;
+    }
+     .description {
+         margin: 0;
+         text-decoration: none;
+         color: #242535;
+         font-size: 16px;
+         font-weight: 400;
+         line-height: 22px;
+     }
+     .tags {
+         line-height: 16px;
+         font-weight: 500;
+         font-style: italic;
+         list-style-type: none;
+         display: flex;
+         flex-wrap: wrap;
+         padding-left: 0;
+     }
+     .tag {
+         font-size: 12px;
+         color: rgba(0, 0, 0, 0.5);
+         margin-right: 8px;
+     }
 </style>
 
 <ul class="jobs">
     {#each jobs as job}
-        <li class="block-heading">
-            {job.position} at {job.name}
-            <p class="description">
-                {@html job.description}
-            </p>
-            <div class="additional">
-                <span>{job.from} - {job.to}</span>
+        <li class="job">
+            <p class="heading">{job.position} at {job.name}</p>
+            <div class="dates">
+                <span class="date">{job.from} - {job.to}</span>
             </div>
+            <ul class="description-block">
+                {#each job.description as desc}
+                    <li class="description">{desc}</li>
+                {/each}
+            </ul>
+            <ul class="tags">
+                {#each job.tags as tag}
+                    <li class="tag">{tag}</li>
+                {/each}
+            </ul>
         </li>
     {/each}
 </ul>
